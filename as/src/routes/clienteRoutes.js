b62537622337bf97f7922e5395116e937b1cad6c
ronaldo <ronidayano0cerca@gmail.com>
2026-01-18 02:57:17 -0300

    Revert "backend subir"
    This reverts commit f77865842eb5d5b67cfe348cf1fe077a20c35849.

--- as/src/routes/clienteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clienteController = require('../controllers/clienteController');
-
-router.get('/comercios', clienteController.getComercios);
-router.post('/clientes', clienteController.crearCliente);
-
-module.exports = router;
